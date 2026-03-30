f979e549f4c3e1f626def78de246c03f8e110186
赵日天 <truenine304520@gmail.com>
2026-03-30 10:15:31 +0800

    Fix CLI native binding test global typing

--- cli/env.d.ts
+++ cli/env.d.ts
@@ -5,14 +5,18 @@
 /**
  * CLI version injected at build time from package.json
  */
-declare const __CLI_VERSION__: string
+declare const __CLI_VERSION__: string;
 
 /**
  * CLI package name injected at build time from package.json
  */
-declare const __CLI_PACKAGE_NAME__: string
+declare const __CLI_PACKAGE_NAME__: string;
 
 /**
  * Kiro global powers registry JSON string injected at build time
  */
-declare const __KIRO_GLOBAL_POWERS_REGISTRY__: string
+declare const __KIRO_GLOBAL_POWERS_REGISTRY__: string;
+
+interface GlobalThis {
+  __TNMSC_TEST_NATIVE_BINDING__?: object;
+}
